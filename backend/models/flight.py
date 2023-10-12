6faf480aadd72353c975930a2aed805a40f4c0c5
NathanielMention <nathanielmention@gmail.com>
2023-10-12 16:44:26 -0400

    add search/filter frontend and backend logic

--- backend/models/flight.py
+++ backend/models/flight.py
@@ -1,6 +1,13 @@
 from models.db import Database
+import psycopg2
+import psycopg2.extras
+
 
 db = Database()
+
+# Access the connection
+conn = db.__conn__
+
 
 class Flight:
     def __init__(self, id, departure, arrival, airline_name, flight_name, flight_status):
@@ -10,7 +17,7 @@ class Flight:
         self.airline_name = airline_name
         self.flight_name = flight_name
         self.flight_status = flight_status
-        
+
     def save_flight_data(self, flight):
         try:
             print(flight.departure)
@@ -20,16 +27,17 @@ class Flight:
                 INSERT INTO flights (departure, arrival, airline_name, flight_name, flight_status) 
                 VALUES (%s, %s, %s, %s, %s)
             """
-            params = (flight.departure, flight.arrival, flight.airline_name, flight.flight_name, flight.flight_status)
-            
+            params = (flight.departure, flight.arrival, flight.airline_name,
+                      flight.flight_name, flight.flight_status)
+
             db.execute(sql_query, params)
             db.commit()
             print("success saving flight to database")
             # cursor.close() not sure if I need this
-            return True  
+            return True
         except Exception as e:
             print(f"Error saving flight: {e}")
-        
+
     @classmethod
     def delete_flight(cls, flight_id):
         try:
@@ -43,7 +51,7 @@ class Flight:
         except Exception as e:
             print(f"Error deleting flight data: {e}")
             return False
-        
+
     def serialize(self):
         return {
             "id": self.id,
@@ -53,7 +61,7 @@ class Flight:
             "flight_name": self.flight_name,
             "flight_status": self.flight_status
         }
-        
+
     @classmethod
     def get_flights(cls):
         try:
@@ -62,24 +70,24 @@ class Flight:
             db.execute(sql_query)
             flights_data = db.fetch_results(True)
             print("here1")
-            #cursor.close()
+            # cursor.close()
             flights = []
             for flight_data in flights_data:
                 flight = cls(
-                id=flight_data['id'], 
-                departure=flight_data['departure'],
-                arrival=flight_data['arrival'],
-                airline_name=flight_data['airline_name'],
-                flight_name=flight_data['flight_name'],
-                flight_status=flight_data['flight_status']
-            )
+                    id=flight_data['id'],
+                    departure=flight_data['departure'],
+                    arrival=flight_data['arrival'],
+                    airline_name=flight_data['airline_name'],
+                    flight_name=flight_data['flight_name'],
+                    flight_status=flight_data['flight_status']
+                )
                 flights.append(flight)
 
             return flights
         except Exception as e:
             print(f"Error fetching flights: {e}")
             return []
-        
+
     @classmethod
     def find_flight(cls, flight_id):
         try:
@@ -93,6 +101,32 @@ class Flight:
                 id, departure, arrival, airline_name, flight_name, flight_status = flight_data
                 return cls(id, departure, arrival, airline_name, flight_name, flight_status)
             else:
-                return None 
+                return None
         except Exception as e:
             print(f"Error finding flight: {e}")
+
+    @classmethod
+    def search_flights(cls, search_query):
+        try:
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            cursor.execute(
+                "SELECT * FROM flights WHERE flight_name ILIKE %s", (f"%{search_query}%",))
+            result = cursor.fetchall()
+            cursor.close()
+            return result
+        except Exception as e:
+            print(f"Error searching flights: {e}")
+            return []
+
+    @classmethod
+    def filter_flights(cls, departure_airport, arrival_airport):
+        try:
+            cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+            query = "SELECT * FROM flights WHERE departure_airport = %s AND arrival_airport = %s"
+            cursor.execute(query, (departure_airport, arrival_airport,))
+            result = cursor.fetchall()
+            cursor.close()
+            return result
+        except Exception as e:
+            print(f"Error filtering flights: {e}")
+            return []
--- frontend/react-template/src/components/FlightPage/index.js
+++ frontend/react-template/src/components/FlightPage/index.js
@@ -1,9 +1,18 @@
-import '../../Styles/navbar.css'
+import '../../styles/navbar.css'
 import React, { useEffect, useState } from 'react'
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 function FlightData() {
   const [flightData, setFlightData] = useState([]);
   const [error, setError] = useState(null);
+  const [searchDeparture, setSearchDeparture] = useState('');
+  const [searchArrival, setSearchArrival] = useState('');
+  const [departureDate, setDepartureDate] = useState(null);
+  const [arrivalDate, setArrivalDate] = useState(null);
+  const [filters, setFilters] = useState({
+    airline: '',
+  });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,6 +33,42 @@ function FlightData() {
   }, []);
 
   console.log(flightData);
+
+  const handleSearch = async () => {
+    try {
+      const departureDateString = departureDate ? departureDate.toISOString().split('T')[0] : '';
+      const arrivalDateString = arrivalDate ? arrivalDate.toISOString().split('T')[0] : '';
+
+      const searchResponse = await fetch(
+        `http://localhost:5000/search-flights?departure=${searchDeparture}&arrival=${searchArrival}&departureDate=${departureDateString}&arrivalDate=${arrivalDateString}`
+      );
+      if (!searchResponse.ok) {
+        throw new Error(`HTTP error! Status: ${searchResponse.status}`);
+      }
+      const searchResult = await searchResponse.json();
+      setFlightData(searchResult.flights);
+    } catch (error) {
+      console.error('Error searching for flights:', error);
+      setError('An error occurred while searching for flights.');
+    }
+  };
+
+  const handleFilter = async () => {
+    try {
+      // Implement filter logic here
+      const filterResponse = await fetch(
+        `http://localhost:5000/api/flights?departure=${filters.departure}&arrival=${filters.arrival}&airline=${filters.airline}`
+      );
+      if (!filterResponse.ok) {
+        throw new Error(`HTTP error! Status: ${filterResponse.status}`);
+      }
+      const filteredData = await filterResponse.json();
+      setFlightData(filteredData);
+    } catch (error) {
+      console.error('Error filtering flights:', error);
+      setError('An error occurred while filtering flights.');
+    }
+  };
     
       return (
         <div className='container'>
@@ -49,7 +94,62 @@ function FlightData() {
             </div>
           </div>
           <div className='right-side'>
-
+          <div>
+          <h2>Flight Search</h2>
+          <input
+            type='text'
+            placeholder='Departure Airport'
+            value={searchDeparture}
+            onChange={(e) => setSearchDeparture(e.target.value)}
+          />
+          <input
+            type='text'
+            placeholder='Arrival Airport'
+            value={searchArrival}
+            onChange={(e) => setSearchArrival(e.target.value)}
+          />
+          <div className='date-pickers'>
+            <div>
+              <label>Departure Date:</label>
+              <DatePicker
+                selected={departureDate}
+                onChange={(date) => setDepartureDate(date)}
+                placeholderText='Select a Date'
+              />
+            </div>
+            <div>
+              <label>Arrival Date:</label>
+              <DatePicker
+                selected={arrivalDate}
+                onChange={(date) => setArrivalDate(date)}
+                placeholderText='Select a Date'
+              />
+            </div>
+          </div>
+          <button onClick={handleSearch}>Search</button>
+        </div>
+            <div>
+            <h2>Flight Filter</h2>
+          <input
+            type='text'
+            placeholder='Departure Airport'
+            value={filters.departure}
+            onChange={(e) => setFilters({ ...filters, departure: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Arrival Airport'
+            value={filters.arrival}
+            onChange={(e) => setFilters({ ...filters, arrival: e.target.value })}
+          />
+          <input
+            type='text'
+            placeholder='Airline'
+            value={filters.airline}
+            onChange={(e) => setFilters({ ...filters, airline: e.target.value })}
+          />
+          <button onClick={handleFilter}>Apply Filters</button>
+            </div>
           </div>
         </div>
       );
